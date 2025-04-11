3a7403da82627c149c6b9f72572e8c5a4b498a12
lovelytotoro1 <unet@lovelytotoro1.com>
2025-04-11 15:35:52 +0800

    commit file with test of gitignore

--- train.py
+++ train.py
@@ -3,15 +3,16 @@ from pathlib import Path
 
 if __name__ == "__main__":
     # Load a model
-    model = YOLO("test_yolo/train/weights/best.pt")  # load an official model
+    # model = YOLO("test_yolo/train/weights/best.pt")  # load an official model
+    model = YOLO("yolov8s.pt")  # load an official model
     # set the model to gpu 5
-    model.to("cuda:5")  # set the model to gpu 5
+    # model.to("cuda:5")  # set the model to gpu 5
 
-    # # Train the model
-    # results = model.train(data="AB_20220608.yaml", epochs=10, imgsz=640, project="test_yolo", device="1")  # train the model
+    # Train the model
+    results = model.train(data="AB_20220608.yaml", epochs=3, imgsz=640, project="test_yolo", name="train99", device="1")  # train the model
     # # Validate the model
     # results = model.val()  # validate the model
-    # # Predict with the model    
+    # Predict with the model    
     results = model.predict(source="/media/chenqunWorkSpace/Project_R/code/YOLOv5/data_test_time")  # predict with the model
 
     for result in results:
